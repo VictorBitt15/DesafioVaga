95c17570931ad2e2bf4066a6ade952ea67f96376
Victor Bittencourt Mafra Veríssimo <07579744490@detran.govrn>
2024-05-14 13:39:31 -0300

    beneficiarios level

---
+++ FI.WebAtividadeEntrevista/Utils/Validacao.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAtividadeEntrevista.Utils
+{
+    public static class Validacao
+    {
+        public static bool ValidaCPF(string cpf)
+        {
+            if (string.IsNullOrEmpty(cpf))
+            {
+                throw new Exception("CPF não informado");
+            }
+
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string tempCpf;
+            string digito;
+            int soma;
+            int resto;
+            cpf = cpf.Trim();
+            cpf = Desmascara(cpf);
+            if (cpf.Length != 11)
+            {
+                return false;
+            }
+
+            tempCpf = cpf.Substring(0, 9);
+            soma = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+            }
+
+            resto = soma % 11;
+            resto = resto < 2 ? 0 : 11 - resto;
+
+            digito = resto.ToString();
+            tempCpf += digito;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            }
+
+            resto = soma % 11;
+            resto = resto < 2 ? 0 : 11 - resto;
+
+            digito += resto.ToString();
+
+            return cpf.EndsWith(digito);
+        }
+        public static string Desmascara(string texto)
+        {
+            return string.IsNullOrEmpty(texto)
+                ? texto
+                : texto.ToString().Replace("-", string.Empty).Replace("_", string.Empty).Replace("/", string.Empty).Replace("\\", string.Empty).Replace(".", string.Empty);
+        }
+    }
+}
